4ff7b506bcf0942dcd2335932175a43187e4975f
kisannaanna <kisanna0610@gmail.com>
2019-05-08 12:47:47 +0200

    Adding css links to head

--- site/R/head.R
+++ site/R/head.R
@@ -1,5 +1,7 @@
 baseHead<-function(){
   tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "myway.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "site.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "base.css"),
             tags$script(src = "customFunctions.js"))
   
 }
