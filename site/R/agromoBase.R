4a3f9376d9d9065a0f71cf3f0ee8d5e85864f8d5
ELTE <ELTE@M-hidyd.agrar.atk>
2019-05-28 15:37:04 +0200

    Base-GUI_first-adjustments

--- site/R/agromoBase.R
+++ site/R/agromoBase.R
@@ -1,19 +1,25 @@
 agromoBaseUI <- function(id){
     ns <- NS(id)
-   tagList(tags$div(id = "base", tags$div(id="components","components"),
+   tagList(tags$div(id = "base", tags$div(id="components","COMPONENTS"),
                tags$div(actionButton("site","", class = "mainMenu", style = ("background: url('img/site.svg')")),
                         actionButton("grid","", class = "mainMenu", style = ("background: url('img/grid.svg')")),
                         actionButton("show","", class = "mainMenu", style = ("background: url('img/show.svg')")),
                         actionButton("map","",  class = "mainMenu", style = ("background: url('img/map.svg')")))
                
                ),
-               tags$div(
+           
+           
+                 tags$div(
                  id = ns("tools"), #base-tools
                  class= "mainSideBar",
-                        tags$div(id="mainSideBarTools", "tools"),
+                        tags$div(id="mainSideBarDirectory", "MAIN DIRECTORY"),
+                        actionButton("choose","CHOOSE", class = "mainSideBar"),
+                        tags$div(id="mainSideBarTools", "TOOLS"),
                         actionButton("parSweep","PARAMETER SWEEP", class = "mainSideBar"),
                         actionButton("sensitivity","SENSITIVITY ANALYSIS", class = "mainSideBar"),
                         actionButton("calibration","CALIBRATION", class = "mainSideBar"),
                         actionButton("storEditor","STORYLINE EDITOR",  class = "mainSideBar"))
            )
+   
+   
 }
