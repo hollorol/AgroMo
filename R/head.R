aced10c55355f939624042b952d760672a8de866
Hollos Roland <hollorol@gmail.com>
2020-08-12 15:14:19 +0200

    language basics

--- R/head.R
+++ R/head.R
@@ -18,8 +18,19 @@ baseHead<-function(){
          shiny::tags$link(rel = "stylesheet", type = "text/css", href = "www/dbman.css"),
          shiny::tags$link(rel = "stylesheet", type = "text/css",
                    href = "www/showTableOutput.css"),
+                   shiny::tags$script(HTML("
+                                          var dictionary;
+                                          let xhttp = new XMLHttpRequest();
+                                          xhttp.onreadystatechange = function(){
+                                              dictionary = JSON.parse(this.responseText);
+                                          }
+                                          xhttp.open(\"GET\", \"www/jsonfiles/agromo_languages.json\",
+                                                     true);
+                                          xhttp.send();
+                                          ")),
          shiny::tags$script(src = "www/jsTable.js"),
          shiny::tags$script(src = "www/customFunctions.js"),
          shiny::tags$script(src = "www/getIndexesFromSelection.js"),
          shiny::tags$script(src="www/jquery-ui-min.js"))
 }
+"inst/"
