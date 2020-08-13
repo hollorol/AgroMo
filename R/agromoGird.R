33594b4b939ab6acbcbca0d828d306df0924b4b2
kisannaanna <50200630+kisannaanna@users.noreply.github.com>
2020-08-13 14:20:09 +0200

    GirdGUI_swithtrue

--- R/agromoGird.R
+++ R/agromoGird.R
@@ -3,15 +3,15 @@
     tags$div(id = ns(id),
     tags$div(
     id = paste0(ns("ensclim"),"_container"),
-    shinyWidgets::switchInput(ns("ensclim"), label = NULL, onLabel="ensemble", offLabel="selected", value = FALSE)
+    shinyWidgets::switchInput(ns("ensclim"), label = NULL, onLabel="ensemble", offLabel="selected", value = TRUE)
     ),
     tags$div(
     id =paste0(ns("ensalg"),"_container"),
-    shinyWidgets::switchInput(ns("ensalg"), label = NULL, onLabel="ensemble", offLabel="selected", value = FALSE)
+    shinyWidgets::switchInput(ns("ensalg"), label = NULL, onLabel="ensemble", offLabel="selected", value = TRUE)
     ),         
     tags$div(
       id =paste0(ns("enssoil"),"_container"),
-      shinyWidgets::switchInput(ns("enssoil"), label = NULL, onLabel="ensemble", offLabel="selected", value = FALSE)
+      shinyWidgets::switchInput(ns("enssoil"), label = NULL, onLabel="ensemble", offLabel="selected", value = TRUE)
     ),
     tags$div(
       id =paste0(ns("repcheck"),"_container"),
@@ -55,7 +55,7 @@
     #),
     tags$div(
       id =paste0(ns("annual"),"_container"),
-      shinyWidgets::switchInput(ns("annual"), label = NULL, onLabel="daily outputs", offLabel="annual outputs", value = FALSE)
+      shinyWidgets::switchInput(ns("annual"), label = NULL, onLabel="daily outputs", offLabel="annual outputs", value = TRUE)
     ),
     tags$div(
       id = paste0(ns("gridres"),"_container"),
